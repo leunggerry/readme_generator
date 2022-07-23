c998ad28d7b9b841a935336da14aba83bfb3d176
gigi.upNorth <leung.gerry@gmail.com>
2022-07-22 22:31:07 -0400

    [node] Adding skeleton files and the inquirer package for the readme generator

---
+++ index.js
@@ -0,0 +1,24 @@
+/** Required Packages
+ **************************************************************************************************/
+const inquirer = require("inquirer");
+
+/** Global Constants
+ **************************************************************************************************/
+// TODO: Create an array of questions for user input
+const questions = [];
+
+/** Function Definitions
+ **************************************************************************************************/
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    console.log("Hello Gerry")
+}
+
+/** Main Function
+ **************************************************************************************************/
+
+// Function call to initialize app
+init();
